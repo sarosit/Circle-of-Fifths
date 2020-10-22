f7b93d89146770f8788807d0bdfe07bb954be0c1
Tomas Sarosi <tommy@tomass-mbp.home>
2020-10-22 12:36:51 +0100

    updated-buttons

--- HomePage/home.js
+++ HomePage/home.js
@@ -24,3 +24,35 @@ document.getElementById('segC').addEventListener('mouseleave', (e) => {
     let toggle = document.getElementById('content-toggle');
     toggle.style.visibility = "hidden";
 });
+
+document.getElementById('content-toggle').addEventListener('mouseenter', (e) => {
+    let toggle = document.getElementById('content-toggle');
+    toggle.style.visibility = "visible";
+});
+
+document.getElementById('content-toggle').addEventListener('mouseleave', (e) => {
+    let toggle = document.getElementById('content-toggle');
+    toggle.style.visibility = "hidden";
+});
+
+// document.getElementById('scales').addEventListener('onclick', (e) => {
+//     document.getElementById('.dropdown-scale-display').style.visibility = 'visible';
+//     document.getElementById('.dropdown-chord-display').style.visibility = 'hidden';
+//     document.getElementById('.user-message').style.visibility = 'hidden';
+// });
+
+// document.getElementById('chords').addEventListener('onclick', (e) => {
+//     document.getElementById('.dropdown-chord-display').style.visibility = 'visible';
+//     document.getElementById('.dropdown-scale-display').style.visibility = 'hidden';
+//     document.getElementById('.user-message').style.visibility = 'hidden';
+// });
+
+
+
+
+var toggle  = document.getElementById("scales");
+var content = document.getElementById(".dropdown-scale-display");
+
+toggle.addEventListener("click", function() {
+  content.style.display = (content.dataset.toggled ^= 1) ? "block" : "none";
+});
