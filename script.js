437e8d4f967f79a7865ed178f1c9e48c41596df9
Tomas Sarosi <tommy@Tomass-MacBook-Pro.local>
2020-12-20 10:52:37 +0000

    update-with-jquery

--- script.js
+++ script.js
@@ -4,7 +4,9 @@ let toggleScale = document.getElementById("scales");
 let contentScale = document.getElementById("scale-display");
 let toggle = document.getElementById("content-toggle");
 let userMess = document.getElementById("user-message");
-let seg = document.getElementsByClassName("segment");
+
+
+
 
 // document.getElementById('C').addEventListener('mouseenter', (e) => {
 //     let x = e.clientX;
@@ -33,23 +35,52 @@ let seg = document.getElementsByClassName("segment");
 // }
 
 
-document.addEventListener('click', function () {
-    for (let i = 0; i < 7; i++) {
-        let parentScale = document.createElement("SPAN");
-        let tonicScale = getNamedScale(getTonicsFromID());
-        parentScale.innerHTML = tonicScale[i];
-        document.getElementById('ionian').appendChild(parentScale);
-    };
-}, {once : true});
+let circle = document.getElementsByClassName("segment");
 
 
+// circle.addEventListener("click", function () {
+
+//     let note = this.id;
+//     let tonicScale = getNamedScale(note);
+//     scaleBox = document.createElement("div");
+//     contentScale.appendChild(scaleBox);
+
+//     contentScale.style.visibility = (contentScale.dataset.toggled ^= 1) ? "visible" : "hidden";
+//     contentChord.style.visibility = "hidden"
+//     userMess.style.visibility = "hidden"
+
+//     for (let i = 0; i < 7; i++) {
+//         let parentScale = document.createElement("SPAN");
+//         parentScale.innerHTML = tonicScale[i];
+//         scaleBox.appendChild(parentScale);
+//     };
+// }, { once: true });
 
 
-toggleScale.addEventListener("click", function () {
+$(".segment").one('click', function () {
+    let note = this.id;
+    let tonicScale = getNamedScale(note);
+    scaleBox = document.createElement("div");
+    contentScale.appendChild(scaleBox);
     contentScale.style.visibility = (contentScale.dataset.toggled ^= 1) ? "visible" : "hidden";
     contentChord.style.visibility = "hidden"
     userMess.style.visibility = "hidden"
+    for (let i = 0; i < 7; i++) {
+        let parentScale = document.createElement("SPAN");
+        parentScale.innerHTML = tonicScale[i];
+        scaleBox.appendChild(parentScale);
+    };
+
 });
+
+
+
+
+// circle.addEventListener("click", function () {
+//     contentScale.style.visibility = (contentScale.dataset.toggled ^= 1) ? "visible" : "hidden";
+//     contentChord.style.visibility = "hidden"
+//     userMess.style.visibility = "hidden"
+// });
 
 toggleChord.addEventListener("click", function () {
     contentChord.style.visibility = (contentChord.dataset.toggled ^= 1) ? "visible" : "hidden";
